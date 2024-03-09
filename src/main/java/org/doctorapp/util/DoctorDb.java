b8a0f9946cf6804e4bdae6da2fe54aa7bbf339ba
AbdulGuranni <1rn19is001.abdulguranni@gmail.com>
2024-03-09 19:18:55 +0530

    Doctor app commit

---
+++ src/main/java/org/doctorapp/util/DoctorDb.java
@@ -0,0 +1,22 @@
+package org.doctorapp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DoctorDb {
+    static Connection connection;
+
+    public static Connection openConnection() {
+        String url = "jdbc:mysql://localhost:3306/voyatraining";
+        String username = "root";
+        String password = "";
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
